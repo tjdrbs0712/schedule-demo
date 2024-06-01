89888d7b81a67b0e909aff81e05c81c133499a7b
SeongKyun <yooss135@naver.com>
2024-06-01 15:55:40 +0900

    git init

---
+++ src/main/java/com/sparta/scheduleproject2/ScheduleProject2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.scheduleproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleProject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/scheduleproject2/ScheduleProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.scheduleproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
